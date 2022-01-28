e7f177ef9dea82bbd170973c17c9f19a0cb91c2b
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-28 16:25:06 -0500

    Add files via upload

---
+++ LeapYears.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//EXAMPLE -- Feel free to remove --
+	for (int year = 1; year <= 10; year++) {
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			cout << year << " was a leap year" << endl;
+		}
+	}
+
+	// PROBLEM 1
+
+
+	// PROBLEM 2
+
+
+	// PROBLEM 3
+
+
+	// PROBLEM 4
+
+
+	// PROBLEM 5
+
+
+	// PROBLEM 6 -- EXTRA CREDIT --
+
+	system("pause");
+}
+
