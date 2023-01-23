c582e149ef6af422235781ed53ddb66b6ca6341a
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-01-23 11:08:39 -0500

    Update LeapYears.cpp

--- LeapYears.cpp
+++ LeapYears.cpp
@@ -3,12 +3,39 @@ using namespace std;
 
 int main()
 {
-	// EXAMPLE: Displays the leap years for the years 1 to 10 
-	// -- Feel free to delete --
-	for (int year = 1; year <= 10; year++) {
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-			cout << year << " was a leap year" << endl;
-		}
+	// 3.5.1 Leap Years Introduction
+	if (2000 % 4 == 0 && 2000 % 100 != 0 || 2000 % 400 == 0) {
+		cout << 2000 << " was a leap year" << endl;
+	}
+	if (2001 % 4 == 0 && 2001 % 100 != 0 || 2001 % 400 == 0) {
+		cout << 2001 << " was a leap year" << endl;
+	}
+	if (2002 % 4 == 0 && 2002 % 100 != 0 || 2002 % 400 == 0) {
+		cout << 2002 << " was a leap year" << endl;
+	}
+	if (2003 % 4 == 0 && 2003 % 100 != 0 || 2003 % 400 == 0) {
+		cout << 2003 << " was a leap year" << endl;
+	}
+	if (2004 % 4 == 0 && 2004 % 100 != 0 || 2004 % 400 == 0) {
+		cout << 2004 << " was a leap year" << endl;
+	}
+	if (2005 % 4 == 0 && 2005 % 100 != 0 || 2005 % 400 == 0) {
+		cout << 2005 << " was a leap year" << endl;
+	}
+	if (2006 % 4 == 0 && 2006 % 100 != 0 || 2006 % 400 == 0) {
+		cout << 2006 << " was a leap year" << endl;
+	}
+	if (2007 % 4 == 0 && 2007 % 100 != 0 || 2007 % 400 == 0) {
+		cout << 2007 << " was a leap year" << endl;
+	}
+	if (2008 % 4 == 0 && 2008 % 100 != 0 || 2008 % 400 == 0) {
+		cout << 2008 << " was a leap year" << endl;
+	}
+	if (2009 % 4 == 0 && 2009 % 100 != 0 || 2009 % 400 == 0) {
+		cout << 2009 << " was a leap year" << endl;
+	}
+	if (2010 % 4 == 0 && 2010 % 100 != 0 || 2010 % 400 == 0) {
+		cout << 2010 << " was a leap year" << endl;
 	}
 
 	// PROBLEM 1
